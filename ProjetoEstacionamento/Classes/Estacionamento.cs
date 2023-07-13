e5b234ff0f9a5e6995f3626ca57433470a9cfc97
Vinicius <vinihnunes7@gmail.com>
2023-07-12 23:38:15 -0300

    Projeto Finalizado

---
+++ ProjetoEstacionamento/Classes/Estacionamento.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEstacionamento.Classes
+{
+    internal class Estacionamento
+    {
+        public double PrecoEstacionamento { get; set; }
+        public double PrecoHora { get; set; }
+        public List<string> Veiculos { get; set; } = new List<string>();
+
+        public Estacionamento(double precoEstacionamento, double precoHora)
+        {
+            PrecoEstacionamento = precoEstacionamento;
+            PrecoHora = precoHora;
+        }
+
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            Veiculos.Add(Console.ReadLine());
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para remove-lo:");
+            string placa = Console.ReadLine();
+
+            if (Veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado: ");
+                int hora = int.Parse(Console.ReadLine());
+
+                double valorTotal = PrecoEstacionamento + PrecoHora * hora;
+
+                Veiculos.Remove(placa);
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+            }
+
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
+        }
+
+        public void ListarVeiculo()
+        {
+            if (Veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são");
+                foreach (string veiculos in Veiculos)
+                {
+                    Console.WriteLine(veiculos);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+    }
+}
---
+++ ProjetoEstacionamento/Program.cs
@@ -0,0 +1,50 @@
+﻿using ProjetoEstacionamento.Classes;
+
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento! \n" +
+                  "Digite o preço inicial: ");
+double precoEstacionamento = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Agora digite o preço por hora: ");
+double precoHora = Convert.ToDouble(Console.ReadLine());
+
+Estacionamento es = new Estacionamento(precoEstacionamento, precoHora);
+
+bool exibirMenu = true;
+
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção: ");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículo");
+    Console.WriteLine("4 - Encerrar");
+    int opcao = Convert.ToInt32(Console.ReadLine());
+
+
+    switch (opcao)
+    {
+        case 1:
+            es.AdicionarVeiculo();
+            break;
+
+        case 2:
+            es.RemoverVeiculo();
+            break;
+
+        case 3:
+            es.ListarVeiculo();
+            break;
+
+        case 4:
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+    Console.WriteLine("Pressione uma tecla para contiuar");
+    Console.ReadLine();
+}
+Console.WriteLine("O programa se encerrou");
